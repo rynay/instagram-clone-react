06a8b3db64858017f34da28385b94be4ac3584b9
Rynay <feirs911@gmail.com>
2021-05-04 19:21:23 +0300

    initialized firebase and added to context

---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: 'AIzaSyATld-u6gqi5EjqFYhPr-DdwcxgfcCGxGw',
+  authDomain: 'instagram-clone-react-cb1af.firebaseapp.com',
+  projectId: 'instagram-clone-react-cb1af',
+  storageBucket: 'instagram-clone-react-cb1af.appspot.com',
+  messagingSenderId: '594930572939',
+  appId: '1:594930572939:web:f7422881a0ea7814e73e8e',
+};
+
+const firebase = Firebase.initializeApp(config);
+const FieldValue = firebase.firestore;
+
+export { firebase, FieldValue };
