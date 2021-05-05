5a5dc530024f1d801acd8089cd2b641c3ff44c9f
Rynay <feirs911@gmail.com>
2021-05-05 16:01:26 +0300

    login page style changes

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -22,54 +22,56 @@ const Login = () => {
 
   return (
     <main className={`container ${s.container}`}>
-      <div className={s.phoneImage}>
-        <img
-          className={s.phoneImage__image}
-          alt=""
-          src="./images/iphone-with-profile.jpg"
-        />
-      </div>
-      <section className={s.content}>
-        <div className={s.content__login}>
-          <div className={s.content__logo}>
-            <img alt="Instagram" src="./images/logo.png" />
-          </div>
-          <form
-            className={`${s.content__form} ${s.form}`}
-            onSubmit={handleSubmit}
-            method="POST"
-          >
-            <input
-              className={s.form__input}
-              type="text"
-              placeholder="Email address"
-              aria-label="Enter your email address here"
-              value={email}
-              onChange={({ target: { value } }) => setEmail(value)}
-            />
-            <input
-              className={s.form__input}
-              type="password"
-              placeholder="Password"
-              aria-label="Enter your password here"
-              value={password}
-              onChange={({ target: { value } }) => setPassword(value)}
-            />
-
-            <button className={s.form__button} disabled={!isValid}>
-              Login
-            </button>
-          </form>
+      <div className={s.wrapper}>
+        <div className={s.phoneImage}>
+          <img
+            className={s.phoneImage__image}
+            alt=""
+            src="./images/iphone-with-profile.jpg"
+          />
         </div>
-        <section className={s.content__signUp}>
-          <p>
-            Don't have an account?
-            <Link to="/sign-up" className={s.content__link}>
-              Sign Up
-            </Link>
-          </p>
+        <section className={s.content}>
+          <div className={s.content__login}>
+            <div className={s.content__logo}>
+              <img alt="Instagram" src="./images/logo.png" />
+            </div>
+            <form
+              className={`${s.content__form} ${s.form}`}
+              onSubmit={handleSubmit}
+              method="POST"
+            >
+              <input
+                className={s.form__input}
+                type="text"
+                placeholder="Email address"
+                aria-label="Enter your email address here"
+                value={email}
+                onChange={({ target: { value } }) => setEmail(value)}
+              />
+              <input
+                className={s.form__input}
+                type="password"
+                placeholder="Password"
+                aria-label="Enter your password here"
+                value={password}
+                onChange={({ target: { value } }) => setPassword(value)}
+              />
+
+              <button className={s.form__button} disabled={!isValid}>
+                Login
+              </button>
+            </form>
+          </div>
+          <section className={s.content__signUp}>
+            <p>
+              Don't have an account?
+              <Link to="/sign-up" className={s.content__link}>
+                Sign Up
+              </Link>
+            </p>
+          </section>
         </section>
-      </section>
+      </div>
     </main>
   );
 };
