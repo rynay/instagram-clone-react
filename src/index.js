3d0558479e9fe5cb7f7bcf3d50a9125fe289b287
Rynay <feirs911@gmail.com>
2021-05-05 15:17:19 +0300

    basic styles reset

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App.js';
 import FirebaseContext from './context/firebaseContext';
 import { firebase, FieldValue } from './lib/firebase';
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
