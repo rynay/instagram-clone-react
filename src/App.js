b437fa5349b8751bd65e5e08600f46a364ce1f6c
Rynay <feirs911@gmail.com>
2021-05-06 11:35:10 +0300

    UserContext + localStorage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useContext, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import Header from './components/Header';
+import FirebaseContext from './context/firebaseContext';
+import UserContext from './context/userContext';
 
 const Login = lazy(() => import('./pages/Login/index'));
 const SignUp = lazy(() => import('./pages/SignUp/index'));
@@ -9,23 +11,40 @@ const Dashboard = lazy(() => import('./pages/Dashboard/index'));
 const NotFound = lazy(() => import('./pages/NotFound/index'));
 
 function App() {
+  const { firebase } = useContext(FirebaseContext);
+  const [user, setUser] = useState(localStorage.getItem('authUser'));
+
+  useEffect(() => {
+    firebase.auth().onAuthStatusChanges((user) => {
+      if (user) {
+        localStorage.setItem('authUser', JSON.stringify(user));
+        setUser(user);
+      } else {
+        localStorage.removeItem('authUser');
+        setUser(null);
+      }
+    });
+  }, [firebase]);
+
   return (
-    <Router>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Switch>
-          <Route path={ROUTES.LOGIN} component={Login} />
-          <Route path={ROUTES.SIGN_UP} component={SignUp} />
-          <Route path={ROUTES.DASHBOARD} exact>
-            <Header />
-            <Dashboard />
-          </Route>
-          <Route>
-            <Header />
-            <NotFound />
-          </Route>
-        </Switch>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={user}>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <Route path={ROUTES.LOGIN} component={Login} />
+            <Route path={ROUTES.SIGN_UP} component={SignUp} />
+            <Route path={ROUTES.DASHBOARD} exact>
+              <Header />
+              <Dashboard />
+            </Route>
+            <Route>
+              <Header />
+              <NotFound />
+            </Route>
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,21 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import UserContext from '../../context/userContext';
+import * as ROUTES from '../../constants/routes';
+import s from './Header.module.scss';
+
 const Header = () => {
-  return <p>Hello From Header!</p>;
+  const user = useContext(UserContext);
+  return (
+    <header className={s.header}>
+      <div className={s.header__logo}>
+        <Link to={user ? ROUTES.DASHBOARD : ROUTES.LOGIN}>
+          <img alt="Instagram" src="./images/logo.png" />
+        </Link>
+      </div>
+      <nav></nav>
+    </header>
+  );
 };
 
 export default Header;
