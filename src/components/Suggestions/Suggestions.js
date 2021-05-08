8067cdbfa358c5b0ebca256271018c9ac0b714af
Rynay <feirs911@gmail.com>
2021-05-08 11:23:28 +0300

    Sidebar with Profile and Suggestions are done

--- src/components/Suggestions/Suggestions.js
+++ src/components/Suggestions/Suggestions.js
@@ -1,15 +1,24 @@
+import Skeleton from 'react-loading-skeleton';
+import { Link } from 'react-router-dom';
+import s from './Suggestions.module.scss';
+
 const Suggestions = ({ suggestions, follow }) => {
   return (
-    <section>
-      <h2>Suggestions for you</h2>
-      <ul>
+    <section className={s.container}>
+      {!suggestions.length && <Skeleton count={1} height={20} />}
+      {!!suggestions.length && <h2 className={s.title}>Suggestions for you</h2>}
+      <ul className={s.list}>
+        {!suggestions.length && <Skeleton count={1} height={150} />}
         {suggestions.map((sug) => (
-          <li key={sug.username}>
-            <div>
-              <img alt="" src={`./images/avatars/${sug.username}.jpg`} />
-            </div>
-            <h3>{sug.username}</h3>
+          <li className={s.list_item} key={sug.username}>
+            <Link className={s.link} to={`/p/${sug.username}`}>
+              <div className={s.image_container}>
+                <img alt="" src={`./images/avatars/${sug.username}.jpg`} />
+              </div>
+              <h3 className={s.username}>{sug.username}</h3>
+            </Link>
             <button
+              className={s.button}
               onKeyDown={(e) => {
                 if (e.key !== 'Enter') return;
                 follow(sug);
