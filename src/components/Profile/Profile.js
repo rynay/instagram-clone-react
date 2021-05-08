51b11142827fc8f9af57cdecc091053ad7ba7c07
Rynay <feirs911@gmail.com>
2021-05-08 11:02:39 +0300

    Profile Skeleton preloader + styles layout

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,21 @@
-const Profile = () => {
-  return <div>Hello from Profile</div>;
+import Skeleton from 'react-loading-skeleton';
+import { Link } from 'react-router-dom';
+import s from './Profile.module.scss';
+
+const Profile = ({ username }) => {
+  return (
+    <>
+      {!username && <Skeleton count={1} height={60} />}
+      {username && (
+        <Link className={s.container} to={`/p/${username}`}>
+          <div className={s.image_container}>
+            <img alt="" src={`./images/avatars/${username}.jpg`} />
+          </div>
+          <h3 className={s.username}>{username}</h3>
+        </Link>
+      )}
+    </>
+  );
 };
 
 export default Profile;
