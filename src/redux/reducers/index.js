11cc09d392fd29331463456eeedaacac5dc3d7e2
Rynay <feirs911@gmail.com>
2021-05-21 14:42:26 +0300

    targetUserReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import currentUserReducer from './currentUserReducer';
+import targetUserReducer from './targetUserReducer';
 
 const rootReducer = combineReducers({
   currentUser: currentUserReducer,
+  targetUser: targetUserReducer,
 });
 
 export default rootReducer;
--- src/redux/reducers/targetUserReducer.js
+++ src/redux/reducers/targetUserReducer.js
@@ -1,4 +1,12 @@
+import * as TYPES from '../TYPES';
+
 const targetUserReducer = (state, action) => {
   switch (action.type) {
+    case TYPES.SET_TARGET_USER:
+      return action.payload;
+    default:
+      return state;
   }
 };
+
+export default targetUserReducer;
